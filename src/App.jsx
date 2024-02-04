9e52932c5d6f8fc1f060f224ffebcd4ea4b297bc
ALOR <146992317+PoolAlorCh@users.noreply.github.com>
2024-02-04 04:07:53 -0500

    pokedex

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Index from './views/Index'
+import Detalle from './views/Detalle'
+
+function App() {
+
+  return (
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Index/>}/>
+    <Route path='/pokemon/:id' element={<Detalle/>}/>
+   </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
